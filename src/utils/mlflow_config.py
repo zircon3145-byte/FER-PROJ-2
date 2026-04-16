28ec5301bb71dfbb651af6e42098f38f0f237302
Murtaza Hussain <murtaza32004@gmail.com>
2026-04-16 01:37:30 -0700

    Add MLflow setup function for experiment tracking

--- src/utils/mlflow_config.py
+++ src/utils/mlflow_config.py
@@ -1,1 +1,7 @@
 #src/utils/mlflow_config.py
+import mlflow
+
+def setup_mlflow(experiment_name="FER-Emotion-Recognition"):
+    mlflow.set_tracking_uri("sqlite:///mlflow.db")
+
+    experiment = mlflow.get_experiment_by_name(experiment_name)
