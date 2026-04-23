f510ab43436704410b3d8db068c42cd659c3eeb9
Murtaza Hussain <murtaza32004@gmail.com>
2026-04-22 18:47:17 -0700

    Implement test for model lazy loading
    Add test for lazy loading of the model in inference.

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -4,3 +4,15 @@
 def create_dummy_face():
     """Creates a valid grayscale face image"""
     return np.zeros((48, 48), dtype=np.uint8)
+
+
+# =========================
+# Model loading
+# =========================
+def test_get_model_lazy_loading(monkeypatch):
+    """Ensure model loads only when needed"""
+
+    import src.inference.predict as p
+
+    # reset state
+    p.model = None
